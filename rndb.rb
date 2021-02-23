a963a41dd1df7b38883c18ad6e88611459aeb8d1
Jason Hutchens <jasonhutchens@gmail.com>
2021-02-23 11:07:53 +0800

    schema

--- rndb.rb
+++ rndb.rb
@@ -31,7 +31,6 @@ DISTRIBUTIONS = {
     fluff: 0.1
   }
 }
-
 SIZE = 1_000_000
 
 module RnDB
@@ -104,9 +103,11 @@ module RnDB
 
   class Database
     def initialize(seed=Time.now.to_i)
+      raise "database already open" unless Thread.current[:rndb_database].nil?
       @prng = Random
       @seed = seed
       @tables = Hash.new { |h, k| h[k] = { class: nil, size: 0 } }
+      Thread.current[:rndb_database] = self
 
       DISTRIBUTIONS.each do |label, distribution|
         raise unless distribution.values.sum == 1
@@ -128,10 +129,10 @@ module RnDB
       @prng = prng
     end
 
-    def get_seed(table, col, row)
-      raise "#{table} - no such table" if @tables[table][:class].nil?
-      raise "#{row} - row out of range" if row < 0 || row >= @tables[table][:size]
-      value = [@seed, table, col, row].join('-')
+    def get_seed(name, col, row)
+      raise "#{table} - no such table" if Thread.current[:rndb_tables][name][:class].nil?
+      raise "#{row} - row out of range" if row < 0 || row >= Thread.current[:rndb_tables][name][:size]
+      value = [@seed, name, col, row].join('-')
       digest = Digest::SHA256.hexdigest(value)
       digest.to_i(16) % 18446744073709551616
     end
@@ -144,9 +145,8 @@ module RnDB
       Faker::Config.random = @prng
     end
 
-    def add_table(name, klass, size)
-      @tables[name][:class] = klass
-      @tables[name][:size] = size
+    def add_table(klass, size)
+      klass.migrate(size)
     end
 
     def generate(table, row)
@@ -273,31 +273,108 @@ module RnDB
   end
 
   class Table
-    def initialize
+    def initialize(id)
+      @id = id
     end
 
-    def self.column(name, args)
-      # TODO: store mapping in thread
+    def attributes
+      _generate_all
     end
 
-    def all
-      # TODO: return a query
+    def to_h
+      attributes
     end
 
-    def where(constraints={})
-      # TODO: return a query
+    def to_s
+      to_h.to_s
+    end
+
+    def self.table_name
+      self.name.downcase.to_sym
+    end
+
+    def self.count
+      schema[:size]
+    end
+
+    def self.find(id)
+      self.new(id)
+    end
+
+    def self.schema
+      Thread.current[:rndb_tables] ||= Hash.new do |tables, name|
+        tables[name] = {
+          class: nil,
+          size: 0,
+          columns: Hash.new do |columns, key|
+            columns[key] = {
+              distribution: nil,
+              generator: nil
+            }
+          end,
+        }
+      end
+      Thread.current[:rndb_tables][table_name]
+    end
+
+    def self.migrate(size)
+      raise "table already added" unless schema[:class].nil?
+      schema[:class] = self
+      schema[:size] = size
+    end
+
+    # TODO: add generator for each property with accessor that caches results in
+    #       attributes, and add attributes method that generates everything
+    def self.column(property, *args)
+      args.each do |arg|
+        index =
+          case arg
+          when Hash
+            :distribution
+          when Proc
+            :generator
+          else
+            raise "bad argument"
+          end
+        schema[:columns][property][index] = arg
+      end
+    end
+
+    private
+
+    def _generate_all
+      @_attributes = {
+        id: @id
+      }
     end
   end
 end
 
 class Ball < RnDB::Table
-  column :colour, { red: 0.3, green: 0.1, brown: 0.01, blue: 0.5, orange: 0.09 }
-  column :transparent, { true => 0.1, false => 0.9 }
-  column :weight_name, { light: 0.3, medium: 0.6, heavy: 0.1 }
-  column :material, { leather: 0.2, steel: 0.4, wood: 0.3, fluff: 0.1 }
-  column :weight, -> id do
+  column :colour, {
+    red: 0.3,
+    green: 0.1,
+    brown: 0.01,
+    blue: 0.5,
+    orange: 0.09
+  }
+  column :transparent, {
+    true => 0.1,
+    false => 0.9
+  }
+  column :weight, {
+    light: 0.3,
+    medium: 0.6,
+    heavy: 0.1
+  }, -> id, value do
     42
   end
+  column :material, {
+    leather: 0.2,
+    steel: 0.4,
+    wood: 0.3,
+    fluff: 0.1
+  }
   column :name, -> id do
     "Fred Smith"
   end
@@ -305,16 +382,15 @@ end
 
 DB = RnDB::Database.new(137)
 DB.set_prng(Crypt::ISAAC)
-DB.add_table(:ball, Ball, 1_000_000)
+DB.add_table(Ball, 1_000_000)
 
-#puts Ball.count
-#puts Ball.find(13)
+puts JSON.pretty_generate(Thread.current[:rndb_tables])
+
+puts Ball.count
+ball = Ball.find(13)
+puts ball
+
 #puts Ball.find_by(weight: 42)
 #puts Ball.filter_by(:name) { |name| name =~ /John/ }.take(3).to_a
-#puts Ball.query(:colour => [:red, :blue], :material => :wood).sample(30).pluck(:weight_name)
-
-puts DB.generate(:ball, 0)
-puts DB.generate(:ball, 1)
-query = DB.query(:ball, :colour => [:red, :blue], :material => :wood)
-puts query.length
-puts query.lazy.take(3).to_a
+#puts Ball.where(:colour => [:red, :blue], :material => :wood).sample(30).pluck(:weight_name)
+#puts Ball.where(:colour => [:red, :blue]).where(:material => :wood).sample(30).pluck(:weight_name)
